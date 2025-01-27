c63693382c5734493e72c1462a2795fc927b8faa
borbasb47 <borbasb47@gmail.com>
2025-01-27 13:20:08 +0100

    Add files via upload

---
+++ 1017projekt/1017projekt/index.php
@@ -0,0 +1,28 @@
+<?php 
+//session_start();
+
+$parsed = parse_url($_SERVER["REQUEST_URI"]); 
+$path = $parsed["path"];
+
+require_once("api/connection.php");
+
+switch ($path) {
+    case "/1017projekt/views/regisztracio.html":
+        require_once("./views/regisztracio.html");
+        break;
+    case "/1017projekt/":
+        require_once('./views/cimlap.html');
+        break;
+    case "/1017projekt/views/fooldal.html":
+        require_once('./views/fooldal.html');
+        break;
+    case "/1017projekt/views/konyv.html":
+        require_once('./views/konyv.html');
+        break;
+    case "/1017projekt/views/felhasznalo.html":
+        require_once('./views/felhasznalo.html');
+        break;
+    default:
+        echo "Az oldal nem található, <a href='/1017projekt/'>ugrás a címlapra</a>";
+        break;
+}
---
+++ 1017projekt/1017projekt/public/javascripts/cimlapNumOfUsers.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var numOfUsers = document.getElementById("numOfUsers");
+    function NumOfUsers() {
+        fetch('../api/cimlapNumOfUsers.php')   
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                numOfUsers.innerHTML = "Csatlakozz az oldalunk " + data.count + " felhasználójához";
+    });
+    }
+
+    NumOfUsers();
+});
