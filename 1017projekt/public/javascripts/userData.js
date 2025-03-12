3bacd85b8c6e3a72a1b8da5698263aa8899c98bc
borbasb47 <borbasb47@gmail.com>
2025-03-12 17:45:04 +0100

    Add files via upload

--- 1017projekt/public/javascripts/userData.js
+++ 1017projekt/public/javascripts/userData.js
@@ -4,10 +4,20 @@ function displayUserData() {
         .then(adat => {
             console.log(adat.email);
             var userEmailElement = document.getElementById("userEmail");
+            var userZipCodeElement = document.getElementById("userZipCode");
+            var userCityElement = document.getElementById("userCity");
+            var userAddressElement = document.getElementById("userAddress");
+
             userEmailElement.innerHTML+=" "+adat.email;
-            if(
-                
-            )
+            if(adat.iranyitoszam==null){
+                userZipCodeElement.innerHTML+="<input type='text'>"
+            }
+            if(adat.utca==null){
+                userCityElement.innerHTML+="<input type='text'>"
+            }
+            if(adat.hazszam==null){
+                userAddressElement.innerHTML+="<input type='text'>"
+            }
         });
 }
 displayUserData();
